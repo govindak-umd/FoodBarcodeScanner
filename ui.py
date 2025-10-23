f9086acab58996f262cd1952583e19e2609e8333
govindajithkumar <govindajithkumar@gmail.com>
2025-10-23 13:00:42 -0700

    conversion to classes

--- ui.py
+++ ui.py
@@ -9,46 +9,64 @@ from barcode_read import *
 from get_food_db import *
 
 
-def display_main_ui(page: ft.Page, processed_nutritional_info):
-    """
-    function to display the UI and basic info
-    :param page:
-    :param processed_nutritional_info: nutritional info from generated JSON file
-    :return:
-    """
-    page.title = "Nutritional Info"
+class DisplayHMI:
 
-    # add the image of the food item
-    img_url = processed_nutritional_info["image_url"]
-
-    # retrieve the food image
-    food_image = ft.Image(
-        src=img_url,
-        width=300,
-        height=200,
-        fit=ft.ImageFit.CONTAIN,
-    )
-
-    # alignment of the image to be top and center of the page
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(
-        ft.Row(
-            [food_image],
-            alignment=ft.MainAxisAlignment.CENTER,
+    def __init__(self, new_page, processed_nutritional_info):
+        self.page = new_page
+        self.page.title = "Nutritional Info"
+        self.processed_nutritional_info = processed_nutritional_info
+        self.ui_colors = {"low": "green", "moderate": "orange", "high": "red"}
+        self.txt_name = ft.TextField(
+            label=str(processed_nutritional_info["product_name_en"])
         )
-    )
+        self.nutr = ft.Text()
 
-    # TODO - change the name in the text bar to the barcode
-    # txt_name = ft.TextField(label=str(processed_nutritional_info["product_name_en"]))
-    txt_name = ft.TextField(label=str(processed_nutritional_info["product_name_en"]))
+        self.display_main_ui()
 
-    nutr = ft.Text()
+    def display_main_ui(self):
+        """
+        function to display the UI and basic info
+        :param page:
+        :param processed_nutritional_info: nutritional info from generated JSON file
+        :return:
+        """
 
-    # ui severity - TODO - Move to an outside JSON
-    ui_colors = {"low": "green", "moderate": "orange", "high": "red"}
+        # add the image of the food item
+        img_url = self.processed_nutritional_info["image_url"]
 
-    def display_nutrition(e):
+        # retrieve the food image
+        food_image = ft.Image(
+            src=img_url,
+            width=300,
+            height=200,
+            fit=ft.ImageFit.CONTAIN,
+        )
+
+        # alignment of the image to be top and center of the page
+        self.page.vertical_alignment = ft.MainAxisAlignment.START
+        self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        self.page.add(
+            ft.Row(
+                [food_image],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        )
+
+        # TODO - change the name in the text bar to the barcode
+        # txt_name = ft.TextField(label=str(self.processed_nutritional_info["product_name_en"]))
+        self.txt_name = ft.TextField(
+            label=str(self.processed_nutritional_info["product_name_en"])
+        )
+
+        self.page.add(
+            self.txt_name,
+            ft.ElevatedButton(
+                "Display Nutritional Info", on_click=self.display_nutrition
+            ),
+            self.nutr,
+        )
+
+    def display_nutrition(self, e):
         """
         function to get nutritional value upon click of the elevated button
         :param e:  Event - mouse click
@@ -60,33 +78,27 @@ def display_main_ui(page: ft.Page, processed_nutritional_info):
 
         spans.append(
             ft.TextSpan(
-                f"{"Serving Size"} - {processed_nutritional_info["serving_size"]}\n",
+                f"{"Serving Size"} - {self.processed_nutritional_info["serving_size"]}\n",
                 style=ft.TextStyle(color="white", size=16),
             )
         )
-        for nutrient_key, nutrient_val in processed_nutritional_info[
+        for nutrient_key, nutrient_val in self.processed_nutritional_info[
             "nutrient_levels"
         ].items():
             # retrieve unit of the nutrient measurement from the nutriments dictionary
-            nutrient_unit = processed_nutritional_info["nutriments"][
+            nutrient_unit = self.processed_nutritional_info["nutriments"][
                 str(nutrient_key + "_unit")
             ]
 
             # add text and set color for the text based on value
-            color = ui_colors.get(nutrient_val, "white")
+            color = self.ui_colors.get(nutrient_val, "white")
             spans.append(
                 ft.TextSpan(
                     f"{nutrient_key.capitalize()} - {nutrient_val} - "
-                    f"{processed_nutritional_info['nutriments'][nutrient_key]} {nutrient_unit}\n",
+                    f"{self.processed_nutritional_info['nutriments'][nutrient_key]} {nutrient_unit}\n",
                     style=ft.TextStyle(color=color, size=16),
                 )
             )
 
-        nutr.spans = spans
-        page.update()
-
-    page.add(
-        txt_name,
-        ft.ElevatedButton("Display Nutritional Info", on_click=display_nutrition),
-        nutr,
-    )
+        self.nutr.spans = spans
+        self.page.update()
