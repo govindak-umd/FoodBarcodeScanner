2e7b7b82eeb06120f665ebeba37c96f45a410fb7
govindajithkumar <govindajithkumar@gmail.com>
2025-10-23 12:17:06 -0700

    main py file to run

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 main script
 """
+
 from ui import *
 from barcode_read import *
 from get_food_db import *
 
 
-ft.app(target=DisplayUI)
+if __name__ == "__main__":
+    ft.app(target=lambda page: display_main_ui(page, nutritional_info))
