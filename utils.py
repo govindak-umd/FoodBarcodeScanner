6e74040ed31631d4a46bc806b72143b12e62abaf
govindajithkumar <govindajithkumar@gmail.com>
2025-10-24 11:53:45 -0700

    barcode validity checker added for numerical barcodes

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ Commonly used utilities file
 """
 
 import json
+import re
 
 
 def check_json_file(food_barcode):
@@ -18,5 +19,19 @@ def check_json_file(food_barcode):
             json.load(f)
             return True
     except FileNotFoundError:
-        print(f"File not found for - {food_barcode}")
+        print(f"File not found in history for - {food_barcode}")
+        return False
+
+
+def barcode_validity_checker(barcode_input):
+    """
+    Function to check if a barcode is valid numerical input
+    :param barcode_input: Barcode input from user in the text box
+    :return: True if Yes, False if No
+    """
+    if re.fullmatch(r"\d+", barcode_input):
+        print(f"Barcode Updated - {barcode_input} is a valid numerical barcode")
+        return True
+    else:
+        print(f"Barcode Update Failed - {barcode_input} is not a number")
         return False
