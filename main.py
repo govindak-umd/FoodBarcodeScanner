eb63c5a6b2b5258b48f85bcd63ad9d4f121f8789
govindajithkumar <govindajithkumar@gmail.com>
2025-10-24 11:50:06 -0700

    code change and ckean up

--- main.py
+++ main.py
@@ -10,24 +10,6 @@ from get_food_db import *
 if __name__ == "__main__":
     sample_barcode = "859213005001"
     sample_barcode_2 = "722252153258"
+    sample_barcode_3 = "8445290728791"  # kitkat
 
-    try:
-        # Retrieve food info from nutrition website
-        food_data = get_website_food_db(sample_barcode)
-        nutritional_info = retrieve_nutrition_data(sample_barcode)
-        get_website_food_db(sample_barcode_2)
-        nutritional_info_2 = retrieve_nutrition_data(sample_barcode_2)
-    except Exception as err:
-        print(err)
-
-    if check_json_file(sample_barcode):
-        try:
-            ft.app(
-                target=lambda page: DisplayHMI(
-                    page, nutritional_info, nutritional_info_2
-                )
-            )
-        except Exception as err:
-            print(err)
-    else:
-        print("JSON file not found - Please try again")
+    ft.app(target=lambda page: DisplayHMI(page))
