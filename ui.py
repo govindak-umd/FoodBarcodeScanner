71d7b2cc38eb79c4e8d6494ae4fa2425693ba3f1
govindajithkumar <govindajithkumar@gmail.com>
2025-11-18 23:54:08 -0500

    new logs and bug fixes

--- ui.py
+++ ui.py
@@ -158,6 +158,7 @@ class DisplayHMI:
                 # Retrieve food info from nutrition website
                 self.food_database_query.get_website_food_db()
             except Exception as err:
+                print("here")
                 logger.error(err)
         self.processed_nutritional_info = (
             self.food_database_query.retrieve_nutrition_data()
@@ -228,6 +229,8 @@ class DisplayHMI:
                 )
             )
 
+            # try displaying the nutrient levels
+
             try:
                 for nutrient_key, nutrient_val in self.processed_nutritional_info[
                     "nutrient_levels"
@@ -259,12 +262,15 @@ class DisplayHMI:
                 color = ui_config["text_color"]["error_text"]
                 spans.append(
                     ft.TextSpan(
-                        "No data available",
+                        "No data available for nutriments",
                         style=ft.TextStyle(
                             color=color, size=ui_config["common_text_size"]
                         ),
                     )
                 )
+
+            # try displaying the nutri_score_color_grade
+
             try:
 
                 ## add text and set color for the text based on value
@@ -288,17 +294,19 @@ class DisplayHMI:
                 )
 
             except KeyError as err:
+
+                logger.error("Key is not present")
                 color = ui_config["text_color"]["error_text"]
                 spans.append(
                     ft.TextSpan(
-                        "No data available",
+                        "No data available for nutri_score_color_grade ",
                         style=ft.TextStyle(
                             color=color, size=ui_config["common_text_size"]
                         ),
                     )
                 )
-                logger.error(err)
-
+                self.nutritional_info.spans = spans
+                self.page.update()
         else:
 
             spans.append(
